aa3757f8f9dbb36b2dcb04f0d5d29ebc3673c84f
Ruihan Yang <2916436728@qq.com>
2025-05-06 17:01:20 +0800

    20250506 添加详情跳转，大搜索框组件化

---
+++ frontend/src/components/SearchBar.vue
@@ -0,0 +1,59 @@
+<script setup>
+import {ref} from "vue";
+
+const searchQuery = ref('')
+</script>
+
+<template>
+    <!--搜索栏-->
+    <div style="margin-top: 50px">
+        <el-row :gutter="10">
+            <el-col :span="2"></el-col>
+            <!--搜索框-->
+            <el-col :span="15" align="center">
+                <input style="height: 60px;width: 100%;font-size: xxx-large;margin-top: 10px;border: none;"
+                       v-model="searchQuery"
+                       placeholder="文物搜索..." @keyup.enter="fetchArtifacts" clearable>
+            </el-col>
+            <!--限制条件-->
+            <el-col :span="2" align="center">
+                <el-dropdown trigger="click"
+                             style="display: flex; align-items: center; justify-content: center; height: 100%;">
+                    <span class="el-dropdown-link">
+                      限制
+                      <el-icon class="el-icon--right">
+                        <arrow-down/>
+                      </el-icon>
+                    </span>
+                    <template #dropdown>
+                        <el-dropdown-menu>
+                            <el-dropdown-item>艺术家</el-dropdown-item>
+                            <el-dropdown-item>标题</el-dropdown-item>
+                            <el-dropdown-item>描述</el-dropdown-item>
+                            <el-dropdown-item>类型</el-dropdown-item>
+                            <el-dropdown-item>含视频</el-dropdown-item>
+                            <el-dropdown-item>朝代</el-dropdown-item>
+                        </el-dropdown-menu>
+                    </template>
+                </el-dropdown>
+            </el-col>
+            <!--搜索按钮-->
+            <el-col :span="3" align="center">
+                <el-button @click="$emit('search', searchQuery)"
+                           style="border-radius: var(--el-border-radius-small);background-color: black;width: 70%;
+                               height: 70px;">
+                    <el-icon style="color: white;font-size: 32px;">
+                        <Search/>
+                    </el-icon>
+                </el-button>
+            </el-col>
+            <el-col :span="2"></el-col>
+        </el-row>
+        <!--分割线-->
+        <el-divider style="border-color: black;width: 85%;margin: 5px auto;border-width: 2px;"></el-divider>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
--- frontend/src/views/HomePage.vue
+++ frontend/src/views/HomePage.vue
@@ -3,52 +3,7 @@
         <el-container direction="vertical" class="p-6">
 
             <!--搜索栏-->
-            <div style="margin-top: 50px">
-                <el-row :gutter="10">
-                    <el-col :span="2"></el-col>
-                    <!--搜索框-->
-                    <el-col :span="15" align="center">
-                        <input style="height: 60px;width: 100%;font-size: xxx-large;margin-top: 10px;border: none;"
-                               v-model="searchQuery"
-                               placeholder="文物搜索..." @keyup.enter="fetchArtifacts" clearable>
-                    </el-col>
-                    <!--限制条件-->
-                    <el-col :span="2" align="center">
-                        <el-dropdown trigger="click"
-                                     style="display: flex; align-items: center; justify-content: center; height: 100%;">
-                    <span class="el-dropdown-link">
-                      限制
-                      <el-icon class="el-icon--right">
-                        <arrow-down/>
-                      </el-icon>
-                    </span>
-                            <template #dropdown>
-                                <el-dropdown-menu>
-                                    <el-dropdown-item>艺术家</el-dropdown-item>
-                                    <el-dropdown-item>标题</el-dropdown-item>
-                                    <el-dropdown-item>描述</el-dropdown-item>
-                                    <el-dropdown-item>类型</el-dropdown-item>
-                                    <el-dropdown-item>含视频</el-dropdown-item>
-                                    <el-dropdown-item>朝代</el-dropdown-item>
-                                </el-dropdown-menu>
-                            </template>
-                        </el-dropdown>
-                    </el-col>
-                    <!--搜索按钮-->
-                    <el-col :span="3" align="center">
-                        <el-button @click="fetchArtifacts"
-                                   style="border-radius: var(--el-border-radius-small);background-color: black;width: 70%;
-                               height: 70px;">
-                            <el-icon style="color: white;font-size: 32px;">
-                                <Search/>
-                            </el-icon>
-                        </el-button>
-                    </el-col>
-                    <el-col :span="2"></el-col>
-                </el-row>
-                <!--分割线-->
-                <el-divider style="border-color: black;width: 85%;margin: 5px auto;border-width: 2px;"></el-divider>
-            </div>
+            <SearchBar @search="handleSearch"></SearchBar>
 
             <!--高级搜索、popular、reset-->
             <div></div>
@@ -61,13 +16,23 @@
                     v-for="artifact in artifacts"
                     :key="artifact.id"
                     class="artifact-card"
+                    @click="goToDetail(artifact.id)"
+                    style="cursor: pointer;"
                 >
                     <el-card style="border: none;border-radius: var(--el-border-radius-small);" shadow="hover">
-                        <img
-                            :src="artifact.image"
-                            class="artifact-image"
-                            alt="artifact image"
-                        />
+                        <el-image :src="artifact.image"
+                                  v-if="artifact.image"
+                                  class="artifact-image"
+                                  alt="artifact image"></el-image>
+                        <el-image v-else>
+                            <template #error>
+                                <div class="image-slot">
+                                    <el-icon>
+                                        <Picture/>
+                                    </el-icon>
+                                </div>
+                            </template>
+                        </el-image>
                         <div class="font-semibold text-base mt-2">{{ artifact.type }}</div>
                         <div class="text-sm text-gray-500">{{ artifact.dynasty }}</div>
                         <div class="text-sm text-gray-500">{{ artifact.matrials }}</div>
@@ -84,18 +49,35 @@
 </template>
 
 <script setup>
-import {ref} from 'vue'
+import {ref, onMounted} from 'vue'
+import {useRouter} from "vue-router";
 import axios from 'axios'
+import SearchBar from '@/components/SearchBar.vue'
 
 const searchQuery = ref('')
 const artifacts = ref([])
 const searched = ref(false)
+const router = useRouter()
+
+const goToDetail = (id) => {
+    router.push(`/detail/${id}`)
+}
+
+const handleSearch = (query) => {
+  searchQuery.value = query
+  fetchArtifacts(query)
+}
+
+
+onMounted(() => {
+    fetchArtifacts();
+})
 
 const fetchArtifacts = async () => {
     searched.value = true
     try {
         const response = await axios.get('http://localhost:5000/search', {
-            params: {q: searchQuery.value}
+            params: {q: searchQuery.value || ''}
         })
         artifacts.value = response.data.results
     } catch (error) {
@@ -132,6 +114,11 @@ const fetchArtifacts = async () => {
     height: auto;
     object-fit: contain;
     display: block;
+}
+
+.image-slot {
+    color: var(--el-text-color-secondary);
+    font-size: 60px;
 }
 
 </style>
