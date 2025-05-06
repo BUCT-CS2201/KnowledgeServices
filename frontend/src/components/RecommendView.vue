55676e2fde4f2eeb223b67da8b0dd53429c3c238
Ruihan Yang <2916436728@qq.com>
2025-05-06 17:33:44 +0800

    20250506 DetailInform分支合并，详情页面样式初步

--- frontend/src/components/RecommendView.vue
+++ frontend/src/components/RecommendView.vue
@@ -1,84 +1,84 @@
 <template>
-  <div class="reco">
-    <div class="left">
-      <div class="l-content">
-        <img src="/image/dog.jpg" alt="">
-        <h3>推荐组件</h3>
-        <p>???</p>
-      </div>
+    <div class="reco">
+        <div class="left">
+            <div class="l-content">
+                <img src="/image/dog.jpg" alt="">
+                <h3>推荐组件</h3>
+                <p>???</p>
+            </div>
+        </div>
+        <div class="right">
+            <div class="theme">
+                <div class="title">
+                    <h1>Theme</h1>
+                </div>
+                <ul>
+                    <li>
+                        <img src="/image/dog.jpg" alt="">
+                        <h3>主要内容介绍</h3>
+                    </li>
+                    <li>
+                        <img src="/image/dog.jpg" alt="">
+                        <h3>主要内容介绍</h3>
+                    </li>
+                    <li>
+                        <img src="/image/dog.jpg" alt="">
+                        <h3>主要内容介绍</h3>
+                    </li>
+                    <li>
+                        <img src="/image/dog.jpg" alt="">
+                        <h3>主要内容介绍</h3>
+                    </li>
+                </ul>
+            </div>
+            <div class="author">
+                <div class="title">
+                    <h1>Author</h1>
+                </div>
+                <ul>
+                    <li>
+                        <img src="/image/dog.jpg" alt="">
+                        <h3>主要内容介绍</h3>
+                    </li>
+                    <li>
+                        <img src="/image/dog.jpg" alt="">
+                        <h3>主要内容介绍</h3>
+                    </li>
+                    <li>
+                        <img src="/image/dog.jpg" alt="">
+                        <h3>主要内容介绍</h3>
+                    </li>
+                    <li>
+                        <img src="/image/dog.jpg" alt="">
+                        <h3>主要内容介绍</h3>
+                    </li>
+                </ul>
+            </div>
+            <div class="dynasty">
+                <div class="title">
+                    <h1>Dynasty</h1>
+                </div>
+                <ul>
+                    <li>
+                        <img src="/image/dog.jpg" alt="">
+                        <h3>主要内容介绍</h3>
+                    </li>
+                    <li>
+                        <img src="/image/dog.jpg" alt="">
+                        <h3>主要内容介绍</h3>
+                    </li>
+                    <li>
+                        <img src="/image/dog.jpg" alt="">
+                        <h3>主要内容介绍</h3>
+                    </li>
+                    <li>
+                        <img src="/image/dog.jpg" alt="">
+                        <h3>主要内容介绍</h3>
+                    </li>
+                </ul>
+            </div>
+        </div>
     </div>
-    <div class="right">
-      <div class="theme">
-        <div class="title">
-          <h1>Theme</h1>
-        </div>
-        <ul>
-          <li>
-            <img src="/image/dog.jpg" alt="">
-            <h3>主要内容介绍</h3>
-          </li>
-          <li>
-            <img src="/image/dog.jpg" alt="">
-            <h3>主要内容介绍</h3>
-          </li>
-          <li>
-            <img src="/image/dog.jpg" alt="">
-            <h3>主要内容介绍</h3>
-          </li>
-          <li>
-            <img src="/image/dog.jpg" alt="">
-            <h3>主要内容介绍</h3>
-          </li>
-        </ul>
-      </div>
-      <div class="author">
-        <div class="title">
-          <h1>Author</h1>
-        </div>
-        <ul>
-          <li>
-            <img src="/image/dog.jpg" alt="">
-            <h3>主要内容介绍</h3>
-          </li>
-          <li>
-            <img src="/image/dog.jpg" alt="">
-            <h3>主要内容介绍</h3>
-          </li>
-          <li>
-            <img src="/image/dog.jpg" alt="">
-            <h3>主要内容介绍</h3>
-          </li>
-          <li>
-            <img src="/image/dog.jpg" alt="">
-            <h3>主要内容介绍</h3>
-          </li>
-        </ul>
-      </div>
-      <div class="dynasty">
-        <div class="title">
-          <h1>Dynasty</h1>
-        </div>
-        <ul>
-          <li>
-            <img src="/image/dog.jpg" alt="">
-            <h3>主要内容介绍</h3>
-          </li>
-          <li>
-            <img src="/image/dog.jpg" alt="">
-            <h3>主要内容介绍</h3>
-          </li>
-          <li>
-            <img src="/image/dog.jpg" alt="">
-            <h3>主要内容介绍</h3>
-          </li>
-          <li>
-            <img src="/image/dog.jpg" alt="">
-            <h3>主要内容介绍</h3>
-          </li>
-        </ul>
-      </div>
-    </div>
-  </div>
 </template>
 
 <script setup name="RecommendView">
@@ -87,62 +87,62 @@
 
 <style scoped>
 .reco {
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: space-around;
-  gap: 20px;
-  width: 100%;
-  background-color: #ca3720;
-  margin-top: 20px;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-around;
+    gap: 20px;
+    width: 100%;
+    background-color: #ca3720;
+    margin-top: 20px;
 }
 
 .left {
-  flex: 1 1 300px;
-  display: flex;
-  justify-content: center;
-  width: 400px;
-  height: 800px;
-  background-color: #46d33c;
+    flex: 1 1 300px;
+    display: flex;
+    justify-content: center;
+    width: 400px;
+    height: 800px;
+    background-color: #46d33c;
 }
 
 .left .l-content {
-  width: 85%;
-  height: 600px;
-  background-color: #dce613;
+    width: 85%;
+    height: 600px;
+    background-color: #dce613;
 }
 
 .left .l-content img {
-  width: 100%;
-  height: auto;
-  object-fit: cover;
+    width: 100%;
+    height: auto;
+    object-fit: cover;
 }
 
 .right {
-  flex: 2 1 600px;
-  width: 1000px;
-  background-color: pink;
+    flex: 2 1 600px;
+    width: 1000px;
+    background-color: pink;
 }
 
 .right ul {
-  margin-top: 10px;
-  display: flex;
-  justify-content: space-between;
+    margin-top: 10px;
+    display: flex;
+    justify-content: space-between;
 }
 
 .right ul li {
-  list-style: none;
-  flex: 1 1 200px;
+    list-style: none;
+    flex: 1 1 200px;
 }
 
 .right ul li img {
-  width: 100%;
-  height: auto;
-  object-fit: cover;
+    width: 100%;
+    height: auto;
+    object-fit: cover;
 }
 
 .right .theme,
 .right .author,
 .right .dynasty {
-  margin: 10px;
+    margin: 10px;
 }
 </style>
--- frontend/src/views/DetailImformation.vue
+++ frontend/src/views/DetailImformation.vue
@@ -1,23 +1,12 @@
 <template>
-    <!-- <NavigationBar></NavigationBar> -->
-    <!-- <div id="app">
-        <router-view/>
-
-    </div> -->
-    <DetailView />
-    <RecommendView />
+    <DetailView/>
+    <RecommendView/>
 </template>
 
 <script setup>
-// import NavigationBar from "@/components/NavigationBar.vue";
 import DetailView from "@/components/DetailView.vue"
 import RecommendView from "@/components/RecommendView.vue"
 </script>
 
 <style>
-/* 全局样式 */
-/* body {
-    margin: 0;
-    padding: 0;
-} */
 </style>
