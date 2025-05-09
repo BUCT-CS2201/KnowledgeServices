e996df5f2344d0f80f65044c1517296646af7020
Ruihan Yang <2916436728@qq.com>
2025-05-10 02:48:12 +0800

    20250510 删除点赞展示\修改主页普通搜索布局

--- frontend/src/views/HomePage.vue
+++ frontend/src/views/HomePage.vue
@@ -6,34 +6,47 @@
             <SearchBar @search="handleSearch"></SearchBar>
 
             <!--高级搜索、popular、reset-->
-            <div></div>
+            <el-row :gutter="24" style="margin: 10px auto;border: 2px solid black;height: 50px;width: 85%">
+                <el-col :span="2">热门</el-col>
+                <el-col :span="20"></el-col>
+                <el-col :span="2">带视频</el-col>
+            </el-row>
 
             <!--grid\table切换、sortby、显示条数-->
-            <el-menu class="switch_tab" mode="horizontal" :ellipsis="false"
-                     active-text-color="#000" @select="handleMenuSelect" :default-active="activeMenu">
-                <el-menu-item index="1" @click.stop style="pointer-events: none; color: inherit; cursor: default;">显示
-                    {{ artifacts.length }}
-                    个文物
-                </el-menu-item>
-                <el-menu-item index="2" @click="clear()">清空</el-menu-item>
-                <el-sub-menu index="3">
-                    <template #title>排序方式:</template>
-                    <el-menu-item index="3-1">时间：新-旧</el-menu-item>
-                    <el-menu-item index="3-2">时间：旧-新</el-menu-item>
-                    <el-menu-item index="3-3">名称：A-Z</el-menu-item>
-                    <el-menu-item index="3-4">名称：Z-A</el-menu-item>
-                </el-sub-menu>
-                <el-menu-item index="4" @click="switch2Table()" :class="{ 'selected': activeMenu === '4' }">表格
+            <el-row :gutter="24" style="margin: 10px auto;width: 85%">
+                <el-col :span="3" style="margin-top: 5px">显示{{ artifacts.length }}个文物</el-col>
+                <el-col :span="2" @click="clear()" style="margin-top: 5px">清空</el-col>
+                <el-col :span="12"></el-col>
+                <el-col :span="3" style="margin-top: 10px">
+                    <el-dropdown placement="bottom">
+                        <span>
+                          排序方式
+                          <el-icon class="el-icon--right">
+                            <arrow-down/>
+                          </el-icon>
+                        </span>
+                        <template #dropdown>
+                            <el-dropdown-menu>
+                                <el-dropdown-item>时间：新-旧</el-dropdown-item>
+                                <el-dropdown-item>时间：旧-新</el-dropdown-item>
+                                <el-dropdown-item>名称：A-Z</el-dropdown-item>
+                                <el-dropdown-item>名称：Z-A</el-dropdown-item>
+                            </el-dropdown-menu>
+                        </template>
+                    </el-dropdown>
+                </el-col>
+                <el-col :span="2" @click="switch2Table()" :class="['table', isgrid === false ? 'active' : 'inactive']">
+                    表格
                     <el-icon>
                         <Operation/>
                     </el-icon>
-                </el-menu-item>
-                <el-menu-item index="5" @click="switch2Grid()" :class="{ 'selected': activeMenu === '5' }">栅格
+                </el-col>
+                <el-col :span="2" @click="switch2Grid()" :class="['grid', isgrid === true ? 'active' : 'inactive']">栅格
                     <el-icon>
                         <Menu/>
                     </el-icon>
-                </el-menu-item>
-            </el-menu>
+                </el-col>
+            </el-row>
 
             <!--栅格展示视图-->
             <ArtifactGrid v-if="isgrid" :artifacts="artifacts"></ArtifactGrid>
@@ -127,13 +140,6 @@ const switch2Grid = () => {
     window.location.hash = ''; // 移除 Hash（或改为 #grid）
 };
 
-const handleMenuSelect = (index) => {
-    // 只让 Grid/Table 项触发选中样式
-    if (index === '4' || index === '5') {
-        activeMenu.value = index
-    }
-}
-
 //清空搜索条件
 const clear = () => {
     console.log('clear');
@@ -161,39 +167,24 @@ const scrollToTop = () => {
     margin: 0 auto;
 }
 
-/*栅格表格切换栏样式*/
-.el-menu--horizontal > .el-menu-item:nth-child(2) {
-    margin-right: auto;
+/*grid/table切换样式*/
+.table,
+.grid {
+    box-sizing: border-box;
+    transition: margin-top 0.5s ease, border 0.5s ease;
 }
 
-/*取消原menu样式*/
-.switch_tab {
-    --el-menu-hover-text-color: grey;
-    --el-menu-hover-bg-color: white;
-    border: none !important;
-}
-
-.switch_tab .el-sub-menu__title.is-active {
-    border-bottom: none;
-}
-
-.switch_tab .el-sub-menu__title {
-    transition: none;
-}
-
-.switch_tab .el-menu-item.is-active {
-    border-bottom: none;
-}
-
-.switch_tab .el-menu-item{
-    transition: none;
-}
-
-
-/*grid/table被选中*/
-.switch_tab .el-menu-item.selected {
+.active {
     border: 2px solid black;
+    padding: 10px;
+    margin-top: 0;
 }
+
+.inactive {
+    margin-top: 10px; /* ✅ 让未选中项略微下沉 */
+    border: none;
+}
+
 
 /*回到顶部样式*/
 :deep(.el-table__header) {
--- frontend/src/views/SelfProfile.vue
+++ frontend/src/views/SelfProfile.vue
@@ -1,28 +1,28 @@
 <script setup>
 import {inject} from "vue";
 import {useRouter} from "vue-router";
-import { ref } from 'vue'
-import { ElMessage } from 'element-plus'
-import { Plus } from '@element-plus/icons-vue'
+import {ref} from 'vue'
+import {ElMessage} from 'element-plus'
+import {Plus} from '@element-plus/icons-vue'
 // `imageUrl` 是一个响应式数据，用来存储图片的 URL
-    const imageUrl = ref('');
+const imageUrl = ref('');
 
-    // 头像上传成功时的回调函数
-    const handleAvatarSuccess = (response, uploadFile) => {
-      imageUrl.value = URL.createObjectURL(uploadFile.raw);
-    };
+// 头像上传成功时的回调函数
+const handleAvatarSuccess = (response, uploadFile) => {
+    imageUrl.value = URL.createObjectURL(uploadFile.raw);
+};
 
-    // 上传文件之前的校验
-    const beforeAvatarUpload = (rawFile) => {
-      if (rawFile.type !== 'image/jpeg') {
+// 上传文件之前的校验
+const beforeAvatarUpload = (rawFile) => {
+    if (rawFile.type !== 'image/jpeg') {
         ElMessage.error('Avatar picture must be JPG format!');
         return false;
-      } else if (rawFile.size / 1024 / 1024 > 2) {
+    } else if (rawFile.size / 1024 / 1024 > 2) {
         ElMessage.error('Avatar picture size can not exceed 2MB!');
         return false;
-      }
-      return true;
-    };
+    }
+    return true;
+};
 const isLoggedIn = inject('isLoggedIn')
 const router = useRouter()
 
@@ -34,223 +34,213 @@ const logout = () => {
 }
 //修改信息
 // 表单引用
-    // const ruleFormRef = ref()
+// const ruleFormRef = ref()
 const dialogVisible = ref(false)
-const passVisible=ref(false)
-    // // 年龄验证函数
-    // const checkAge = (rule, value, callback) => {
-    //   if (!value) {
-    //     return callback(new Error('Please input the age'))
-    //   }
-    //   setTimeout(() => {
-    //     if (!Number.isInteger(value)) {
-    //       callback(new Error('Please input digits'))
-    //     } else {
-    //       if (value < 18) {
-    //         callback(new Error('Age must be greater than 18'))
-    //       } else {
-    //         callback()
-    //       }
-    //     }
-    //   }, 1000)
-    // }
+const passVisible = ref(false)
+// // 年龄验证函数
+// const checkAge = (rule, value, callback) => {
+//   if (!value) {
+//     return callback(new Error('Please input the age'))
+//   }
+//   setTimeout(() => {
+//     if (!Number.isInteger(value)) {
+//       callback(new Error('Please input digits'))
+//     } else {
+//       if (value < 18) {
+//         callback(new Error('Age must be greater than 18'))
+//       } else {
+//         callback()
+//       }
+//     }
+//   }, 1000)
+// }
 
-    // // 密码验证函数
-    // const validatePass = (rule, value, callback) => {
-    //   if (value === '') {
-    //     callback(new Error('Please input the password'))
-    //   } else {
-    //     if (ruleForm.checkPass !== '') {
-    //       if (!ruleFormRef.value) return
-    //       ruleFormRef.value.validateField('checkPass')
-    //     }
-    //     callback()
-    //   }
-    // }
+// // 密码验证函数
+// const validatePass = (rule, value, callback) => {
+//   if (value === '') {
+//     callback(new Error('Please input the password'))
+//   } else {
+//     if (ruleForm.checkPass !== '') {
+//       if (!ruleFormRef.value) return
+//       ruleFormRef.value.validateField('checkPass')
+//     }
+//     callback()
+//   }
+// }
 
-    // // 确认密码验证函数
-    // const validatePass2 = (rule, value, callback) => {
-    //   if (value === '') {
-    //     callback(new Error('Please input the password again'))
-    //   } else if (value !== ruleForm.pass) {
-    //     callback(new Error("Two inputs don't match!"))
-    //   } else {
-    //     callback()
-    //   }
-    // }
+// // 确认密码验证函数
+// const validatePass2 = (rule, value, callback) => {
+//   if (value === '') {
+//     callback(new Error('Please input the password again'))
+//   } else if (value !== ruleForm.pass) {
+//     callback(new Error("Two inputs don't match!"))
+//   } else {
+//     callback()
+//   }
+// }
 
-    // // 表单数据
-    // const ruleForm = reactive({
-    //   pass: '',
-    //   checkPass: '',
-    //   age: '',
-    // })
+// // 表单数据
+// const ruleForm = reactive({
+//   pass: '',
+//   checkPass: '',
+//   age: '',
+// })
 
-    // // 表单验证规则
-    // const rules = reactive({
-    //   pass: [{ validator: validatePass, trigger: 'blur' }],
-    //   checkPass: [{ validator: validatePass2, trigger: 'blur' }],
-    //   age: [{ validator: checkAge, trigger: 'blur' }],
-    // })
+// // 表单验证规则
+// const rules = reactive({
+//   pass: [{ validator: validatePass, trigger: 'blur' }],
+//   checkPass: [{ validator: validatePass2, trigger: 'blur' }],
+//   age: [{ validator: checkAge, trigger: 'blur' }],
+// })
 
-    // // 提交表单
-    // const submitForm = (formEl) => {
-    //   if (!formEl) return
-    //   formEl.validate((valid) => {
-    //     if (valid) {
-    //       console.log('submit!')
-    //     } else {
-    //       console.log('error submit!')
-    //     }
-    //   })
-    // }
+// // 提交表单
+// const submitForm = (formEl) => {
+//   if (!formEl) return
+//   formEl.validate((valid) => {
+//     if (valid) {
+//       console.log('submit!')
+//     } else {
+//       console.log('error submit!')
+//     }
+//   })
+// }
 
-    // // 重置表单
-    // const resetForm = (formEl) => {
-    //   if (!formEl) return
-    //   formEl.resetFields()
-    // }
+// // 重置表单
+// const resetForm = (formEl) => {
+//   if (!formEl) return
+//   formEl.resetFields()
+// }
 </script>
 
 <template>
     <div class="common-layout">
-      <el-container>
-        <el-aside width="200px" class="aside">
-            <h1>个人信息</h1>
-          <!-- 头像 -->
-          <el-upload
-                class="avatar-uploader"
-                action="https://run.mocky.io/v3/9d059bf9-4660-45f2-925d-ce80ad6c4d15"
-                :show-file-list="false"
-                :on-success="handleAvatarSuccess"
-                :before-upload="beforeAvatarUpload"
-              >
-                <img v-if="imageUrl" :src="imageUrl" class="avatar" />
-                <el-icon v-else class="avatar-uploader-icon"><Plus /></el-icon>
-              </el-upload>
-
-        <!-- 修改信息 -->
-         <el-button plain @click="dialogVisible = true">
-                修改信息
-        </el-button>
-
-        <el-dialog v-model="dialogVisible" :modal="false" class="el-dia">
-            <div class="title">
-                    <h1>用户信息修改</h1>
-                </div>
-            <el-form
-                    ref="ruleFormRef"
-                    style="max-width: 600px"
-                    :model="ruleForm"
-                    status-icon
-                    :rules="rules"
-                    label-width="auto"
-                    class="demo-ruleForm"
+        <el-container>
+            <el-aside width="200px" class="aside">
+                <h1>个人信息</h1>
+                <!-- 头像 -->
+                <el-upload
+                    class="avatar-uploader"
+                    action="https://run.mocky.io/v3/9d059bf9-4660-45f2-925d-ce80ad6c4d15"
+                    :show-file-list="false"
+                    :on-success="handleAvatarSuccess"
+                    :before-upload="beforeAvatarUpload"
                 >
-                
-                    <el-form-item label="用户名：" prop="user">
-                    <el-input type="username" autocomplete="off" />
-                    </el-form-item>
-                    <el-form-item label="简介：" prop="descr">
-                    <el-input
-                        v-model="textarea"
-                        :rows="2"
-                        type="textarea"
-                        placeholder="Please input"
-                       class="no-resize"
-                    />
-                    </el-form-item>
-                    <el-form-item label="性别：" prop="gender">
-                    <el-input />
-                    </el-form-item>
-                    <el-form-item label="地址：" prop="address">
-                    <el-input />
-                    </el-form-item>
-                    <el-form-item label="年龄：" prop="age">
-                    <el-input />
-                    </el-form-item>
-                    <el-form-item label="微信：" prop="wechat">
-                    <el-input />
-                    </el-form-item>
-                    <el-form-item label="QQ：" prop="qq">
-                    <el-input />
-                    </el-form-item>
-                </el-form>
-            <template #footer>
-            <div class="dialog-footer">
-                <el-button @click="dialogVisible = false">Cancel</el-button>
-                <el-button type="primary" @click="dialogVisible = false">
-                Confirm
+                    <img v-if="imageUrl" :src="imageUrl" class="avatar"/>
+                    <el-icon v-else class="avatar-uploader-icon">
+                        <Plus/>
+                    </el-icon>
+                </el-upload>
+
+                <!-- 修改信息 -->
+                <el-button plain @click="dialogVisible = true">
+                    修改信息
                 </el-button>
-            </div>
-            </template>
-        </el-dialog>
-          <!-- 信息展示 -->
-              <el-card style="max-width: 480px">
-                <template #header>
-                  <div class="card-header">
-                    <span>Card name</span>
-                  </div>
-                </template>
-                <p v-for="o in 4" :key="o" class="text item">{{ 'List item ' + o }}</p>
-                <template #footer>Footer content</template>
-              </el-card>
-        
-         
-        </el-aside>
-        <!-- 主要部分 -->
-        <el-main class="el-main-demo">
-          <!-- 菜单栏 -->
-           <el-tabs type="border-card" class="demo-tabs">
-              <el-tab-pane>
-                <template #label>
+                <el-dialog v-model="dialogVisible" :modal="false" class="el-dia">
+                    <div class="title">
+                        <h1>用户信息修改</h1>
+                    </div>
+                    <el-form
+                        ref="ruleFormRef"
+                        style="max-width: 600px"
+                        :model="ruleForm"
+                        status-icon
+                        :rules="rules"
+                        label-width="auto"
+                        class="demo-ruleForm"
+                    >
+
+                        <el-form-item label="用户名：" prop="user">
+                            <el-input type="username" autocomplete="off"/>
+                        </el-form-item>
+                        <el-form-item label="简介：" prop="descr">
+                            <el-input
+                                v-model="textarea"
+                                :rows="2"
+                                type="textarea"
+                                placeholder="Please input"
+                                class="no-resize"
+                            />
+                        </el-form-item>
+                        <el-form-item label="性别：" prop="gender">
+                            <el-input/>
+                        </el-form-item>
+                        <el-form-item label="地址：" prop="address">
+                            <el-input/>
+                        </el-form-item>
+                        <el-form-item label="年龄：" prop="age">
+                            <el-input/>
+                        </el-form-item>
+                        <el-form-item label="微信：" prop="wechat">
+                            <el-input/>
+                        </el-form-item>
+                        <el-form-item label="QQ：" prop="qq">
+                            <el-input/>
+                        </el-form-item>
+                    </el-form>
+                    <template #footer>
+                        <div class="dialog-footer">
+                            <el-button @click="dialogVisible = false">Cancel</el-button>
+                            <el-button type="primary" @click="dialogVisible = false">
+                                Confirm
+                            </el-button>
+                        </div>
+                    </template>
+                </el-dialog>
+                <!-- 信息展示 -->
+                <el-card style="max-width: 480px">
+                    <template #header>
+                        <div class="card-header">
+                            <span>Card name</span>
+                        </div>
+                    </template>
+                    <p v-for="o in 4" :key="o" class="text item">{{ 'List item ' + o }}</p>
+                    <template #footer>Footer content</template>
+                </el-card>
+            </el-aside>
+            <!-- 主要部分 -->
+            <el-main class="el-main-demo">
+                <!-- 菜单栏 -->
+                <el-tabs type="border-card" class="demo-tabs">
+                    <el-tab-pane label="Config">
+                        <template #label>
                   <span class="custom-tabs-label">
-                    <el-icon><Pointer /></el-icon>
-                    <span>点赞</span>
-                  </span>
-                </template>
-                点赞
-              </el-tab-pane>
-              <el-tab-pane label="Config">
-                <template #label>
-                  <span class="custom-tabs-label">
-                    <el-icon><Star /></el-icon>
+                    <el-icon><Star/></el-icon>
                     <span>收藏</span>
                   </span>
-                </template>
-                收藏
-              </el-tab-pane>
-              <el-tab-pane label="Role">
-                <template #label>
+                        </template>
+                        收藏
+                    </el-tab-pane>
+                    <el-tab-pane label="Role">
+                        <template #label>
                   <span class="custom-tabs-label">
-                    <el-icon><ChatDotSquare /></el-icon>
+                    <el-icon><ChatDotSquare/></el-icon>
                     <span>评论</span>
                   </span>
-                </template>
-                评论
-              </el-tab-pane>
-          </el-tabs>
-        </el-main>
-      </el-container>
-  </div>
+                        </template>
+                        评论
+                    </el-tab-pane>
+                </el-tabs>
+            </el-main>
+        </el-container>
+    </div>
     <el-button @click="logout">登出</el-button>
     <!-- 修改密码 -->
     <el-button plain @click="passVisible = true">
-    修改密码
-  </el-button>
-  <el-dialog v-model="passVisible" :modal="false">
-    <el-form-item label="密码：" prop="pass">
-        <el-input type="password" autocomplete="off" />
-    </el-form-item>
-    <template #footer>
-      <div class="dialog-footer">
-        <el-button @click="passVisible = false">Cancel</el-button>
-        <el-button type="primary" @click="passVisible = false">
-          Confirm
-        </el-button>
-      </div>
-    </template>
-  </el-dialog>
+        修改密码
+    </el-button>
+    <el-dialog v-model="passVisible" :modal="false">
+        <el-form-item label="密码：" prop="pass">
+            <el-input type="password" autocomplete="off"/>
+        </el-form-item>
+        <template #footer>
+            <div class="dialog-footer">
+                <el-button @click="passVisible = false">Cancel</el-button>
+                <el-button type="primary" @click="passVisible = false">
+                    Confirm
+                </el-button>
+            </div>
+        </template>
+    </el-dialog>
 </template>
 
 <style scoped>
@@ -265,32 +255,38 @@ const passVisible=ref(false)
     font-size: larger;
     border-bottom: none !important;
 }
+
 .avatar-uploader .avatar {
-  width: 100px;
-  height: 100px;
-  display: block;
+    width: 100px;
+    height: 100px;
+    display: block;
 }
-.el-menu-popper-demo{
-  display: flex;
-  justify-content: space-evenly;
+
+.el-menu-popper-demo {
+    display: flex;
+    justify-content: space-evenly;
 }
+
 .el-dia .title {
-  display: flex;
-  justify-content: center; /* 水平居中 */
-  align-items: center;     /* 垂直居中 */
-  height: 50px;            /* 设置高度使内容垂直居中 */
+    display: flex;
+    justify-content: center; /* 水平居中 */
+    align-items: center; /* 垂直居中 */
+    height: 50px; /* 设置高度使内容垂直居中 */
 }
+
 .el-dia {
     display: flex;
     justify-content: center;
     align-items: center;
 }
+
 .demo-ruleForm {
-  width: 100%;             /* 让表单宽度占满容器 */
-  max-width: 600px;        /* 最大宽度 */
-  margin: auto;            /* 自动设置外边距以实现居中 */
+    width: 100%; /* 让表单宽度占满容器 */
+    max-width: 600px; /* 最大宽度 */
+    margin: auto; /* 自动设置外边距以实现居中 */
 }
-.dialog-footer{
+
+.dialog-footer {
     display: flex;
     justify-content: center;
 }
@@ -298,39 +294,43 @@ const passVisible=ref(false)
 </style>
 <style>
 .avatar-uploader .el-upload {
-  border: 1px dashed var(--el-border-color);
-  border-radius: 6px;
-  cursor: pointer;
-  position: relative;
-  overflow: hidden;
-  transition: var(--el-transition-duration-fast);
+    border: 1px dashed var(--el-border-color);
+    border-radius: 6px;
+    cursor: pointer;
+    position: relative;
+    overflow: hidden;
+    transition: var(--el-transition-duration-fast);
 }
 
 .avatar-uploader .el-upload:hover {
-  border-color: var(--el-color-primary);
+    border-color: var(--el-color-primary);
 }
 
 .el-icon.avatar-uploader-icon {
-  font-size: 28px;
-  color: #8c939d;
-  width: 100px;
-  height: 100px;
-  text-align: center;
+    font-size: 28px;
+    color: #8c939d;
+    width: 100px;
+    height: 100px;
+    text-align: center;
 }
+
 .demo-tabs > .el-tabs__content {
-  padding: 32px;
-  color: #6b778c;
-  font-size: 32px;
-  font-weight: 600;
+    padding: 32px;
+    color: #6b778c;
+    font-size: 32px;
+    font-weight: 600;
 }
+
 .demo-tabs .custom-tabs-label .el-icon {
-  vertical-align: middle;
+    vertical-align: middle;
 }
+
 .demo-tabs .custom-tabs-label span {
-  vertical-align: middle;
-  margin-left: 4px;
+    vertical-align: middle;
+    margin-left: 4px;
 }
-.demo-ruleForm .no-resize{
+
+.demo-ruleForm .no-resize {
     resize: none !important;
 }
 </style>
